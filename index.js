31dc8d988c00e4467131820c46f5b340d5f727a9
vybhavb <vybs2499@gmail.com>
2016-11-11 18:53:05 -0800

    Setup Firebase so that vicky can meme

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var config = {
+    apiKey: "AIzaSyD9QoCxhv_Nuzkt32ZZSurGfEKvdPvX3-s",
+    authDomain: "verifi-55b09.firebaseapp.com",
+    databaseURL: "https://verifi-55b09.firebaseio.com",
+    storageBucket: "verifi-55b09.appspot.com",
+    messagingSenderId: "969001895612"
+  };
+
 $(window).ready(function() {
   $(".button-collapse").sideNav();
+  firebase.initializeApp(config);
 });
