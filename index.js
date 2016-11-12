5a46d88bed3b9f37e75a24c74347a13bbfa908a9
RTavare <rohit.t797@yahoo.com>
2016-11-11 20:39:02 -0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var config = {
+    apiKey: "AIzaSyD9QoCxhv_Nuzkt32ZZSurGfEKvdPvX3-s",
+    authDomain: "verifi-55b09.firebaseapp.com",
+    databaseURL: "https://verifi-55b09.firebaseio.com",
+    storageBucket: "verifi-55b09.appspot.com",
+    messagingSenderId: "969001895612"
+  };
+
 $(window).ready(function() {
   $(".button-collapse").sideNav();
+  $('.collapsible').collapsible();
+  firebase.initializeApp(config);
 });
